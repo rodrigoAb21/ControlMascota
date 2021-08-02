312a9df1033117671f289b09bf63f3756d711f98
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2021-08-02 18:30:34 -0400

    Primer parte plantilla

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('plugins/fontawesome/css/all.min.css')}}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{asset('icon.png')}}" type="image/x-icon">
+    <title>Control Mascota</title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light bg-gradient">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('icon.png')}}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Control Mascota
+        </a>
+    </div>
+</nav>
+
+
+
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.index');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
