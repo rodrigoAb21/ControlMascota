266052b230e67f90b7310756b8bdc253ffac252c
IngenieriaSoftware2021 <ingenieria.software.uagrm@gmail.com>
2021-08-11 00:46:46 -0400

    error iconox2

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,13 +3,13 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- Bootstrap CSS -->
     <link href="{{asset('plugins/fontawesome/css/all.min.css')}}" rel="stylesheet">
     <link href="{{asset('plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{asset('icon.png')}}" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('icon.png')}}"/>
     <title>Control Mascota</title>
 
     @stack('arriba')
